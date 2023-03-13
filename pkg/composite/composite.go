a745675688cc60fe7f50604af8501e23421c2c42
Phil Winder <phil@Winder.AI>
2023-03-13 21:32:40 +0000

    refactor(composite): start stripping out composite

--- pkg/composite/composite.go
+++ pkg/composite/composite.go
@@ -9,28 +9,29 @@ import (
 
 	"github.com/bacalhau-project/amplify/pkg/executor"
 	"github.com/ipfs/go-cid"
-	ipldformat "github.com/ipfs/go-ipld-format"
 )
 
 // Composite is a combination of an IPLD node and resultant CIDs.
 // It is made thread safe by embedding a sync.Mutex and hiding the fields
 type Composite struct {
 	sync.Mutex
-	node   ipldformat.Node
+	c      cid.Cid
 	result executor.Result
 }
 
 // NewComposite creates a new composite from a root IPLD node represented by a
 // CID
-func NewComposite(ctx context.Context, ng ipldformat.NodeGetter, cid cid.Cid) (*Composite, error) {
-	node, err := ng.Get(ctx, cid)
-	if err != nil {
-		return nil, err
-	}
+func NewComposite(ctx context.Context, cid cid.Cid) (*Composite, error) {
 	c := &Composite{
-		node: node,
+		c: cid,
 	}
 	return c, nil
+}
+
+func (c *Composite) Cid() cid.Cid {
+	c.Lock()
+	defer c.Unlock()
+	return c.c
 }
 
 // String returns a string representation of the composite
@@ -38,13 +39,6 @@ func (c *Composite) String() string {
 	var buf bytes.Buffer
 	print(c, &buf, 0)
 	return buf.String()
-}
-
-// Node returns the Node of a composite
-func (c *Composite) Node() ipldformat.Node {
-	c.Lock()
-	defer c.Unlock()
-	return c.node
 }
 
 // Result gets the result of a composite
@@ -65,8 +59,8 @@ func print(c *Composite, buf *bytes.Buffer, indent int) {
 	c.Lock()
 	defer c.Unlock()
 	if c.result.CID.Defined() {
-		fmt.Fprintf(buf, "%s --> %s\n", c.node.Cid().String(), c.result.CID.String())
+		fmt.Fprintf(buf, "%s --> %s\n", c.c.String(), c.result.CID.String())
 	} else {
-		fmt.Fprintf(buf, "%s\n", c.node.Cid().String())
+		fmt.Fprintf(buf, "%s\n", c.c.String())
 	}
 }
