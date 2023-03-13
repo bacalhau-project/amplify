dc2596aa30ed25557fcde8f37dafb94894da5f1d
Phil Winder <phil@Winder.AI>
2023-03-13 22:11:03 +0000

    refactor(queue): remove old queue components

--- pkg/queue/queue.go
+++ pkg/queue/queue.go
@@ -2,18 +2,7 @@ package queue
 
 import "context"
 
-// Callable encapsulates the method that will be called by a worker to run a job
-type Callable func(context.Context) error
-
-// Queuer encapsulates the Enqueue method that will be called by a dispatcher
-// Deprecated: use Queue instead
-type Queuer interface {
-	Enqueue(QueueRepository, string, Callable) error // Adds item to the queue
-	Start() error                                    // Starts the processing of the queue
-	Stop() error                                     // Stops the processing of the queue
-}
-
-// Queuer encapsulates the Enqueue method that will be called by a dispatcher
+// Queue encapsulates the Enqueue method that will be called by a dispatcher
 type Queue interface {
 	Enqueue(func(context.Context)) error // Adds item to the queue
 	Start()                              // Starts the processing of the queue
