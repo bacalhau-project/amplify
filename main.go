4150a1ac997b4ec9db3d0ec744b0a30123f4d51a
Phil Winder <phil@Winder.AI>
2023-04-20 17:45:11 +0100

    fix(container): add yarn build to container

--- main.go
+++ main.go
@@ -6,12 +6,7 @@ import (
 	"github.com/bacalhau-project/amplify/cmd"
 )
 
-// bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi // an image
-// QmabskAjK5ePM1fTYoUzDTk51LkGdTn2rt26FBj1Q9Qv7T // A bacalhau result
-// bafkreidhrwwuuhipvbqnxi4sgn6ea52psaeria4frzc3js3mkvea53aaq4 // a html file
-
 func main() {
-	// Base context
 	ctx := context.Background()
 	defer ctx.Done()
 
