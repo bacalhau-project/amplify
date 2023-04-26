95aaea4e4ca21a15678762ef18733963a1967831
Enrico Rotundo <enrico.rotundo@gmail.com>
2023-04-26 13:34:16 +0200

    feat(job): frictionless framework for csv metadata extraction and val… (#95)
    * feat(job): frictionless framework for csv metadata extraction and validation
    
    * fix(frictionless): remove unecessary deps in Dockerfile
    
    * fix(frictionless): quiet debug output

---
+++ containers/frictionless/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+IMAGE=ghcr.io/bacalhau-project/amplify/frictionless:latest
+
+echo ${SCRIPT_DIR}
+
+# checkError ensures previous command succeeded
+checkError() {
+    if [ $? -ne 0 ]; then
+        echo "Failed to run test, there was an error"
+        exit 1
+    fi
+}
+
+# checkFileExists ensures that a file exists
+checkFileExists() {
+    if [ ! -f "$1" ]; then
+        echo "File $1 does not exist"
+        exit 1
+    fi
+}
+
+main() {
+    rm -rf $SCRIPT_DIR/outputs
+    docker run -it --rm -v $SCRIPT_DIR/../test/testdata/csv:/inputs -v $SCRIPT_DIR/outputs:/outputs  --entrypoint "" $IMAGE run
+    checkError
+    
+    checkFileExists "$SCRIPT_DIR/outputs/countries-not-valid.csv.metadata.json"
+    checkFileExists "$SCRIPT_DIR/outputs/countries-not-valid.csv.validate.json"
+    checkFileExists "$SCRIPT_DIR/outputs/subdir/country-3-valid.csv.metadata.json"
+    checkFileExists "$SCRIPT_DIR/outputs/subdir/country-3-valid.csv.validate.json"
+}
+
+main
