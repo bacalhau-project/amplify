2f99e63ab9670873a85e24f76b2755542c6fb994
enricorotundo <enrico.rotundo@gmail.com>
2023-04-28 15:31:27 +0200

    fix(misc): addressed PR review comments

--- containers/frictionless-extract/test.sh
+++ containers/frictionless-extract/test.sh
@@ -20,15 +20,30 @@ checkFileExists() {
     fi
 }
 
+# checkFileNotExists ensures that a does NOT file exists
+checkFileNotExists() {
+    if [ -f "$1" ]; then
+        echo "File $1 does exist!"
+        exit 1
+    fi
+}
+
 main() {
     rm -rf $SCRIPT_DIR/outputs
     docker run -it --rm -v $SCRIPT_DIR/../test/testdata/various_tables:/inputs -v $SCRIPT_DIR/outputs:/outputs  --entrypoint "" $IMAGE run
     checkError
-    
     checkFileExists "$SCRIPT_DIR/outputs/table1.csv.csv"
     checkFileExists "$SCRIPT_DIR/outputs/table1.csv.zip.csv"
     checkFileExists "$SCRIPT_DIR/outputs/table1.parquet.csv"
     checkFileExists "$SCRIPT_DIR/outputs/table2.xlsx.csv"
-}
+
+    # test input without extension
+    rm -rf $SCRIPT_DIR/outputs
+    docker run -it --rm -v $SCRIPT_DIR/../test/testdata/csv_blob:/inputs -v $SCRIPT_DIR/outputs:/outputs  --entrypoint "" $IMAGE run
+    checkError
+    checkFileExists "$SCRIPT_DIR/outputs/bank-blob.csv.csv"
+    checkFileNotExists "$SCRIPT_DIR/outputs/image1-blob"
+    checkFileNotExists "$SCRIPT_DIR/outputs/image1-blob.csv"
+}   
 
 main
