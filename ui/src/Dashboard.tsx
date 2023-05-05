134d30b24ce21e31322e7179a872cd03503a7248
Phil Winder <phil@Winder.AI>
2023-05-05 17:11:55 +0100

    chore(docs): add logos and update README

--- ui/src/Dashboard.tsx
+++ ui/src/Dashboard.tsx
@@ -6,90 +6,139 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { Datagrid, List, NumberField, Resource, TextField, Title, useGetList } from 'react-admin';
 import { ResponsiveLine } from '@nivo/line'
+import logoUrl from './assets/amplify_logo_text.svg'
+import Box from '@mui/material/Box';
 
-export default () => (
-    <div>
-        <Title title="Bacalhau Amplify" />
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <Card sx={{ minWidth: 275 }}>
-                    <CardContent>
-                        <Typography variant="h1" component="div">
-                            Bacalhau Amplify
-                        </Typography>
-                        <Typography variant="body2">
-                            Bacalhau Amplify is a decentralized, open-source, and community-driven project to automatically enrich, enhance, and explain data.
-                            <br />
-                            <br />
-                            This is the administrative interface for the Bacalhau Amplify project.
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <a href="https://github.com/bacalhau-project/amplify/">
-                            <Button variant="outlined">Learn More</Button>
-                        </a>
-                        <a href="/#/queue/create">
-                            <Button variant="contained">Submit a Job</Button>
-                        </a>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item sm={12} md={6} lg={4}>
-                <Card>
-                    <CardContent>
-                        <Typography variant="h3" >
-                            Top Content-Type
-                        </Typography>
-                        <Typography variant="body2">
-                            This table shows the top mime-types of all files flowing through Amplify. This data is produced by the metadata-job and stored in the database.
-                        </Typography>
-                        <Resource name="analytics/results/content-type" list={ResultList} hasEdit={false} hasShow={false} hasCreate={false} options={{ label: 'Content-Type' }} />
-                    </CardContent>
-                </Card>
-            </Grid>
-            <Grid item sm={12} md={6} lg={4}>
-                <Card>
-                    <CardContent>
-                        <Typography variant="h3" >
-                            Top Content-Classification
-                        </Typography>
-                        <Typography variant="body2">
-                            This table shows the top object classifications from all images and videos flowing through Amplify. This data is produced by the detection job and stored in the database.
-                        </Typography>
-                        <Resource name="analytics/results/content-classification" list={ResultList} hasEdit={false} hasShow={false} hasCreate={false} options={{ label: 'content-classification' }} />
-                    </CardContent>
-                </Card>
-            </Grid>
-            <Grid item sm={12} md={6} lg={4}>
-                <Card>
-                    <CardContent>
-                        <Typography variant="h3" >
-                            Amplify Metrics
-                        </Typography>
-                        <Typography variant="body2">
-                           This chart shows the number of submissions and node executions over time. These numbers are aggregated based upon the timestamps in the database.
-                        </Typography>
-                        <ContentTypeBarChart />
-                    </CardContent>
-                </Card>
-            </Grid>
+export default () => {
+    const submissionData = useGetList(
+        'analytics/metrics-over-time/submissions',
+        { pagination: { perPage: 1, page: 1 } },
+    );
 
-            <Grid item sm={12} md={12} lg={12}>
-                <Card>
-                    <CardContent>
-                        <Typography variant="h3" >
-                            Most Recent Summaries
-                        </Typography>
-                        <Typography variant="body2">
-                            This table shows the most recent text summaries of the content flowing through Amplify.
-                        </Typography>
-                        <Resource name="analytics/recent-results/summary_text" list={RecentResultList} hasEdit={false} hasShow={false} hasCreate={false} options={{ label: 'summary_text' }} />
-                    </CardContent>
-                </Card>
+    const resultsData = useGetList(
+        'analytics/metrics-over-time/node-results',
+        { pagination: { perPage: 1, page: 1 } },
+    );
+
+    return (
+        <div>
+            <Title title="Bacalhau Amplify" />
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Card sx={{ minWidth: 275 }}>
+                        <CardContent>
+                            <Grid container spacing={2}>
+                                <Grid item xs={2} md={1}>
+                                    <Box
+                                        component="img"
+                                        sx={{
+                                            height: 100,
+                                            width: 100,
+                                            maxHeight: { xs: 233, md: 167 },
+                                            maxWidth: { xs: 350, md: 250 },
+                                        }}
+                                        alt="Amplify Logo"
+                                        src={logoUrl}
+                                    />
+                                </Grid>
+                                <Grid item xs={10} md={11}>
+                                    <Typography variant="h1" component="div" display="block">
+                                        Bacalhau Amplify
+                                    </Typography>
+
+                                </Grid>
+
+                            </Grid>
+                            <Typography variant="body2">
+                                Bacalhau Amplify is a decentralized, open-source, and community-driven project to automatically enrich, enhance, and explain data.
+                                <br />
+                                <br />
+                                This is the administrative interface for the Bacalhau Amplify project.
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <a href="https://github.com/bacalhau-project/amplify/">
+                                <Button variant="outlined">Learn More</Button>
+                            </a>
+                            <a href="/#/queue/create">
+                                <Button variant="contained">Submit a Job</Button>
+                            </a>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item sm={12} md={6} lg={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h3" >
+                                Top Content-Type
+                            </Typography>
+                            <Typography variant="body2">
+                                This table shows the top mime-types of all files flowing through Amplify. This data is produced by the metadata-job and stored in the database.
+                            </Typography>
+                            <Resource name="analytics/results/content-type" list={ResultList} hasEdit={false} hasShow={false} hasCreate={false} options={{ label: 'Content-Type' }} />
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sm={12} md={6} lg={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h3" >
+                                Top Content-Classification
+                            </Typography>
+                            <Typography variant="body2">
+                                This table shows the top object classifications from all images and videos flowing through Amplify. This data is produced by the detection job and stored in the database.
+                            </Typography>
+                            <Resource name="analytics/results/content-classification" list={ResultList} hasEdit={false} hasShow={false} hasCreate={false} options={{ label: 'content-classification' }} />
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sm={12} md={6} lg={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h3" >
+                                Amplify Metrics
+                            </Typography>
+                            <Typography variant="body2">
+                                This chart shows the number of submissions and node executions over time. These numbers are aggregated based upon the timestamps in the database.
+                                {(() => {
+                                    if (submissionData) {
+                                        return (
+                                            <span> Amplify has processed {submissionData.total} CIDs.</span>
+                                        )
+                                    }
+                                    return null;
+                                })()}
+                                {(() => {
+                                    if (resultsData) {
+                                        return (
+                                            <span> Amplify has submitted {resultsData.total} Jobs.</span>
+                                        )
+                                    }
+                                    return null;
+                                })()}
+                            </Typography>
+                            <ContentTypeBarChart />
+                        </CardContent>
+                    </Card>
+                </Grid>
+
+                <Grid item sm={12} md={12} lg={12}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h3" >
+                                Most Recent Summaries
+                            </Typography>
+                            <Typography variant="body2">
+                                This table shows the most recent text summaries of the content flowing through Amplify.
+                            </Typography>
+                            <Resource name="analytics/recent-results/summary_text" list={RecentResultList} hasEdit={false} hasShow={false} hasCreate={false} options={{ label: 'summary_text' }} />
+                        </CardContent>
+                    </Card>
+                </Grid>
             </Grid>
-        </Grid>
-    </div>
-);
+        </div>
+    )
+};
 
 const ResultList = () => (
     <List pagination={false} bulkActionButtons={false} actions={false} title={<div></div>} sort={{ field: 'meta.count', order: 'DESC' }}>
@@ -103,7 +152,7 @@ const ResultList = () => (
 const RecentResultList = () => (
     <List pagination={false} bulkActionButtons={false} actions={false} title={<div></div>} sort={{ field: 'meta.created_at', order: 'DESC' }}>
         <Datagrid rowClick={false} bulkActionButtons={false} >
-            <NumberField source="meta.created_at" noWrap  sortable={false} />
+            <NumberField source="meta.created_at" noWrap sortable={false} />
             <TextField source="id" sortable={false} />
         </Datagrid>
     </List>
@@ -125,45 +174,45 @@ const ContentTypeBarChart = ({ }) => {
     let plotData = [{
         id: "Submissions",
         data: dataA.data.map((item: any) => ({
-        "y": item.meta.count,
-        "x": item.id,
-    }))
-    },{
+            "y": item.meta.count,
+            "x": item.id,
+        }))
+    }, {
         id: "Node Executions",
         data: dataB.data.map((item: any) => ({
-        "y": item.meta.count,
-        "x": item.id,
-    }))
+            "y": item.meta.count,
+            "x": item.id,
+        }))
     }];
 
     return (
         <div style={{ height: 400 }}>
-        <ResponsiveLine
-            data={plotData}
-            xScale={{
-                type: "time",
-                format: "%Y-%m-%dT%H:%M:%SZ",
-                precision: "hour"
-              }}
-            axisBottom={{
-                format: "%H:%M",
-                tickValues: "every 1 hour",
-            }}
-            margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
-            legends={[
-                {
-                    anchor: 'bottom',
-                    translateY: 50,
-                    direction: 'row',
-                    itemWidth: 120,
-                    itemHeight: 20,
-                    itemOpacity: 0.75,
-                    symbolSize: 12,
-                    symbolShape: 'circle',
-                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                }
-            ]}
-        />
+            <ResponsiveLine
+                data={plotData}
+                xScale={{
+                    type: "time",
+                    format: "%Y-%m-%dT%H:%M:%SZ",
+                    precision: "hour"
+                }}
+                axisBottom={{
+                    format: "%H:%M",
+                    tickValues: "every 1 hour",
+                }}
+                margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
+                legends={[
+                    {
+                        anchor: 'bottom',
+                        translateY: 50,
+                        direction: 'row',
+                        itemWidth: 120,
+                        itemHeight: 20,
+                        itemOpacity: 0.75,
+                        symbolSize: 12,
+                        symbolShape: 'circle',
+                        symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                    }
+                ]}
+            />
         </div>
     );
 };
