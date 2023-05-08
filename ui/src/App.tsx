2f6b1e959b3be5c2b44d68271fb719dedb188590
Phil Winder <phil@Winder.AI>
2023-05-08 10:55:57 +0100

    feat(ui): add resources for analytics to show pagination
    * add links to pages in dash

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,10 @@
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import FindInPageIcon from '@mui/icons-material/FindInPage';
+import ImageSearchIcon from '@mui/icons-material/ImageSearch';
+import ShortTextIcon from '@mui/icons-material/ShortText';
 import WorkIcon from '@mui/icons-material/Work';
 import { Admin, Resource, ShowGuesser, defaultTheme } from "react-admin";
+import { ContentList, RecentResultList } from './Analytics';
 import Dashboard from './Dashboard';
 import { GraphList } from './Graph';
 import { JobList } from './Jobs';
@@ -28,6 +32,9 @@ const App = () => (
     <Resource name="queue" list={QueueList} show={QueueItem} create={QueueCreate} options={{ label: 'Queue' }} />
     <Resource name="graph" list={GraphList} options={{ label: 'Graph' }} icon={AccountTreeIcon} />
     <Resource name="jobs" list={JobList} show={ShowGuesser} options={{ label: 'Jobs' }} icon={WorkIcon} />
+    <Resource name="analytics/results/content-type" list={ContentList} hasShow={false} hasCreate={false} hasEdit={false} options={{ label: 'Analytics: Content Types' }} icon={FindInPageIcon} />
+    <Resource name="analytics/results/content-classification" list={ContentList} hasShow={false} hasCreate={false} hasEdit={false} options={{ label: 'Analytics: Content Classifications' }} icon={ImageSearchIcon} />
+    <Resource name="analytics/recent-results/summary_text" list={RecentResultList} hasShow={false} hasCreate={false} hasEdit={false} options={{ label: 'Analytics: Text Summaries' }} icon={ShortTextIcon} />
   </Admin >
 );
 export default App;
